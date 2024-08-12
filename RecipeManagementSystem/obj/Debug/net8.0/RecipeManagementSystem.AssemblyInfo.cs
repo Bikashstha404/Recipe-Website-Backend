24190ee0d165b6b0f8ad9e01000b452d5d4138d6
Bikash Shrestha <stha.bikam99@gmail.com>
2024-08-12 17:36:05 +0545

    Some changes

--- RecipeManagementSystem/obj/Debug/net8.0/RecipeManagementSystem.AssemblyInfo.cs
+++ RecipeManagementSystem/obj/Debug/net8.0/RecipeManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783d287c7638ee29bc03fbf81123a56f323f2497")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
